3ff39bd5edf3e96e884be6d68439577cdb721554
Ainsley Muir <ainsleymuir@gmail.com>
2020-04-06 20:41:45 -0400

    fixed formatting

--- planets.py
+++ planets.py
@@ -1,9 +1,7 @@
 def weight_on_planets():
    earth=float(input('What do you weigh on earth? '))
    
-   print('\nOn Mars you would weigh %.2f pounds. \
-   	     \nOn Jupiter you would weigh %.2f pounds.' \
-   	     %(earth*0.38, earth*2.34))
+   print('\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds.' %(earth*0.38, earth*2.34))
    
    
 if __name__ == '__main__':
